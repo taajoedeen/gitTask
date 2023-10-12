2733c629207d2be4fbec5410599a1562329b11d7
Taajoedeen <taajoedeen2@gmail.com>
2023-10-12 22:21:49 +0200

    Modified helloWorld.js to accept user input

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
-console.log("Git is Awesome!");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Please enter some data: ", (data) => {
+  console.log("You entered: " + data);
+  rl.close();
+});
